1f896a3e0f14b4dfd5ac3f23e305843a1691e34d
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2025-01-07 16:44:07 -0300

    feat: add english language.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  staticImage: true,
+  flexsearch: {
+    codeblocks: false,
+  },
+  defaultShowCopyCode: true,
 });
 
 module.exports = withNextra({
   i18n: {
-    locales: [/* "en",  */ "pt"],
-    defaultLocale: "pt",
+    locales: ["en", "pt"],
+    defaultLocale: "en",
+    localeDetection: false,
   },
 });
 
